8dfabfaf960801181e6e3295c809e20c5511cad5
Mårten Wikström <marten.wikstrom@sigmatechnology.se>
2021-09-17 16:37:37 +0200

    toggle text style

--- src/internal/input-handlers/toggle-text-style.ts
+++ src/internal/input-handlers/toggle-text-style.ts
@@ -1,15 +1,24 @@
-import { TextStyleProps } from "scribing";
+import { TextStyle, TextStyleProps } from "scribing";
 import { getSelectionFromInput } from "./get-selection-from-input";
 import { InputHandler } from "./InputHandler";
 
 /** @internal */
-export const toggleTextStyle = (key: keyof TextStyleProps): InputHandler => (event, host, state) => {
+export const toggleTextStyle = (
+    key: BooleanTextStyleKeys,
+): InputHandler => (event, host, state) => {
     const { content, theme } = state;
     const selection = getSelectionFromInput(event, host);
+    
     if (selection === null) {
         return null;
     }
-    // TODO: IMPLEMENT
-    // const effective = selection.getEffectiveTextStyle(content, theme);
-    return null;
+    
+    const current = selection.getUniformTextStyle(content, theme).get(key);
+    const apply = TextStyle.empty.set(key, !current);
+
+    return selection.formatText(apply);
 };
+
+type BooleanTextStyleKeys = {
+    [K in keyof TextStyleProps]-?: boolean extends TextStyleProps[K] ? K : never
+}[keyof TextStyleProps];
