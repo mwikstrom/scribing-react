5fe391abdf8d242ec4779865d77d061c4713f641
Mårten Wikström <marten.wikstrom@sigmatechnology.se>
2021-09-21 00:57:06 +0200

    break styles

--- src/LineBreakView.tsx
+++ src/LineBreakView.tsx
@@ -1,21 +1,40 @@
-import React from "react";
+import clsx from "clsx";
+import React, { useMemo } from "react";
 import { createUseStyles } from "react-jss";
 import { LineBreak } from "scribing";
 import { flowNode } from "./FlowNodeComponent";
 import { makeJssId } from "./internal/utils/make-jss-id";
+import { getTextStyleClassNames, TEXT_STYLE_CLASSES } from "./internal/utils/text-style-to-classes";
+import { getTextCssProperties } from "./internal/utils/text-style-to-css";
 
-export const LineBreakView = flowNode<LineBreak>((_, ref) => {
+export const LineBreakView = flowNode<LineBreak>((props, ref) => {
+    const { node, theme } = props;
+    const { style: givenStyle } = node;
+    const style = useMemo(() => {
+        let ambient = theme.getAmbientTextStyle();
+        if (givenStyle.link) {
+            ambient = ambient.merge(theme.getLinkStyle());
+        }
+        return ambient.merge(givenStyle);
+    }, [givenStyle, theme]);
+    const css = useMemo(() => getTextCssProperties(style), [style]);
     const classes = useStyles();
+    const className = useMemo(
+        () => clsx(classes.root, ...getTextStyleClassNames(style, classes)),
+        [style, classes]
+    );
     return (
         <span
             ref={ref}
-            className={classes.root}
+            className={className}
+            style={css}
             children={"↵\n"}
         />
     );
 });
 
 const useStyles = createUseStyles({
+    ...TEXT_STYLE_CLASSES,
     root: {
         opacity: 0.5,
         whiteSpace: "pre",
--- src/ParagraphBreakView.tsx
+++ src/ParagraphBreakView.tsx
@@ -1,21 +1,33 @@
-import React from "react";
+import clsx from "clsx";
+import React, { useMemo } from "react";
 import { createUseStyles } from "react-jss";
 import { ParagraphBreak } from "scribing";
 import { flowNode } from "./FlowNodeComponent";
 import { makeJssId } from "./internal/utils/make-jss-id";
+import { getTextStyleClassNames, TEXT_STYLE_CLASSES } from "./internal/utils/text-style-to-classes";
+import { getTextCssProperties } from "./internal/utils/text-style-to-css";
 
-export const ParagraphBreakView = flowNode<ParagraphBreak>((_, ref) => {
+export const ParagraphBreakView = flowNode<ParagraphBreak>((props, ref) => {
+    const { theme } = props;
+    const style = theme.getAmbientTextStyle();
+    const css = useMemo(() => getTextCssProperties(style), [style]);
     const classes = useStyles();
+    const className = useMemo(
+        () => clsx(classes.root, ...getTextStyleClassNames(style, classes)),
+        [style, classes]
+    );
     return (
         <span
             ref={ref}
-            className={classes.root}
+            className={className}
+            style={css}
             children="¶"
         />
     );
 });
 
 const useStyles = createUseStyles({
+    ...TEXT_STYLE_CLASSES,
     root: {
         opacity: 0.5,
     },
