df516da26423119754e48dbc6742a5685d0f3032
Mårten Wikström <marten.wikstrom@sigmatechnology.se>
2021-09-22 11:34:31 +0200

    handle end key

--- src/FlowEditor.tsx
+++ src/FlowEditor.tsx
@@ -137,6 +137,26 @@ export const FlowEditor: FC<FlowEditorProps> = props => {
             }
         }
 
+        // Handle case when moving caret to end to avoid ending up after a paragraph break
+        if (e.key === "End" && !e.ctrlKey && !e.shiftKey && state.selection && state.selection.isCollapsed) {
+            const newSelection = state.selection.transformRanges((range, options = {}) => {
+                const { target } = options;
+                if (target) {
+                    const cursor = target.peek(range.last).findNodeForward(n => n instanceof ParagraphBreak);
+                    if (cursor?.node instanceof ParagraphBreak) {
+                        return range.translate(cursor.position - range.last);
+                    }
+                }
+                return null;
+            }, { target: state.content });
+
+            if (newSelection) {
+                e.preventDefault();
+                applyChange(state.set("selection", newSelection));
+                return;
+            }
+        }
+
         // Handle case when moving caret left to avoid ending up after a paragraph break
         if (e.key === "ArrowLeft" && state.selection && state.selection.isCollapsed) {
             const newSelection = state.selection.transformRanges((range, options = {}) => {
