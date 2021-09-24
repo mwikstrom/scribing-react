e9c5a940bea1871d226c8f4cee059b119fb4efa6
Mårten Wikström <marten.wikstrom@sigmatechnology.se>
2021-09-24 13:41:25 +0200

    nested flow selection

--- src/internal/mapping/flow-selection-to-dom.ts
+++ src/internal/mapping/flow-selection-to-dom.ts
@@ -1,20 +1,42 @@
-import { FlowSelection, FlowRangeSelection } from "scribing";
+import { FlowSelection, FlowRangeSelection, NestedFlowSelection, FlowButtonSelection } from "scribing";
 import { mapFlowPositionToDom } from "./flow-position-to-dom";
 
 /** @internal */
 export function mapFlowSelectionToDom(
     flowSelection: FlowSelection | null,
-    editingHost: HTMLElement,
+    container: Node,
     domSelection: Selection,
 ): void {
-    if (flowSelection instanceof FlowRangeSelection) {
-        const { range } = flowSelection;
-        const domAnchor = mapFlowPositionToDom(range.anchor, editingHost);
-        const domFocus = range.isCollapsed ? domAnchor : mapFlowPositionToDom(range.focus, editingHost);
-        if (domAnchor && domFocus) {
-            domSelection.setBaseAndExtent(domAnchor.node, domAnchor.offset, domFocus.node, domFocus.offset);
-            return;
+    let mapped = false;
+
+    while (flowSelection instanceof NestedFlowSelection) {
+        const parent = mapFlowPositionToDom(flowSelection.position, container);
+        
+        if (parent === null) {
+            break;
+        }
+
+        if (flowSelection instanceof FlowButtonSelection) {
+            container = parent.node.childNodes.item(parent.offset);
+            flowSelection = flowSelection.content;
+        } else {
+            break;
         }
     }
-    domSelection.removeAllRanges();
+
+    if (flowSelection instanceof FlowRangeSelection) {
+        const { range } = flowSelection;
+        const domAnchor = mapFlowPositionToDom(range.anchor, container);
+        const domFocus = range.isCollapsed ? domAnchor : mapFlowPositionToDom(range.focus, container);
+        if (domAnchor && domFocus) {
+            domSelection.setBaseAndExtent(domAnchor.node, domAnchor.offset, domFocus.node, domFocus.offset);
+            mapped = true;
+        }
+    } else {
+        console.warn("Unsupported flow selection type");
+    }
+
+    if (!mapped) {
+        domSelection.removeAllRanges();
+    }
 }
