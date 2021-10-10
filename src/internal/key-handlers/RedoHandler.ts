99e2673c597191314af2262d8dc9e89361bd7002
Mårten Wikström <5657237+mwikstrom@users.noreply.github.com>
2021-10-10 03:56:30 +0200

    special undo/redo keys

--- src/internal/key-handlers/RedoHandler.ts
+++ src/internal/key-handlers/RedoHandler.ts
@@ -2,7 +2,7 @@ import { KeyHandler } from "./KeyHandler";
 
 export const RedoHandler: KeyHandler = (e, state) => {
     // CTRL + Y redoes last undone operation
-    if (e.code === "KeyY" && e.ctrlKey && !e.shiftKey && !e.altKey) {
+    if ((e.code === "KeyY" && e.ctrlKey && !e.shiftKey && !e.altKey) || e.key === "Redo") {
         return state.redo();
     }
 };
--- src/internal/key-handlers/UndoHandler.ts
+++ src/internal/key-handlers/UndoHandler.ts
@@ -2,7 +2,7 @@ import { KeyHandler } from "./KeyHandler";
 
 export const UndoHandler: KeyHandler = (e, state) => {
     // CTRL + Z undoes last operation
-    if (e.code === "KeyZ" && e.ctrlKey && !e.shiftKey && !e.altKey) {
+    if ((e.code === "KeyZ" && e.ctrlKey && !e.shiftKey && !e.altKey) || e.key === "Undo") {
         return state.undo();
     }
 };
