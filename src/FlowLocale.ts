14804f1aef388ef6b9929276f10227730b393676
Mårten Wikström <5657237+mwikstrom@users.noreply.github.com>
2021-10-14 11:33:41 +0200

    paragraph variant locale

--- src/FlowLocale.ts
+++ src/FlowLocale.ts
@@ -1,11 +1,32 @@
+import { ParagraphStyleVariant } from "scribing";
+
 /** @public */
-export interface FlowLocale {
+export interface FlowLocale extends Record<ParagraphVariantLocaleKey, string> {
     hold_ctrl_key_to_enable_interaction: string;
     script_error: string;
 }
 
 /** @public */
+export type ParagraphVariantLocaleKey = `paragraph_variant_${ParagraphStyleVariant}`;
+
+/** @public */
 export const DefaultFlowLocale: Readonly<FlowLocale> = Object.freeze({
     hold_ctrl_key_to_enable_interaction: "Hold CTRL key to enable interaction",
     script_error: "Script error",
+    paragraph_variant_normal: "Normal",
+    paragraph_variant_h1: "Heading 1",
+    paragraph_variant_h2: "Heading 2",
+    paragraph_variant_h3: "Heading 3",
+    paragraph_variant_h4: "Heading 4",
+    paragraph_variant_h5: "Heading 5",
+    paragraph_variant_h6: "Heading 6",
+    paragraph_variant_title: "Title",
+    paragraph_variant_subtitle: "Subtitle",
+    paragraph_variant_preamble: "Preamble",
+    paragraph_variant_code: "Code",
 });
+
+/** @public */
+export function getParagraphVariantLocaleKey(variant: ParagraphStyleVariant): ParagraphVariantLocaleKey {
+    return `paragraph_variant_${variant}`;
+}
--- src/internal/tools/ParagraphVariantButton.tsx
+++ src/internal/tools/ParagraphVariantButton.tsx
@@ -3,13 +3,17 @@ import React, { FC } from "react";
 import { ToolButton } from "../ToolButton";
 import { mdiFormatText, mdiMenuDown } from "@mdi/js";
 import { ToolbarProps } from "../Toolbar";
+import { getParagraphVariantLocaleKey, useFlowLocale } from "../..";
 
 export const ParagraphVariantButton: FC<ToolbarProps> = ({commands}) => {
     const variant = commands.getParagraphVariant();
+    const locale = useFlowLocale();
+    const localeKey = variant ? getParagraphVariantLocaleKey(variant) : undefined;
+    const label = localeKey ? locale[localeKey] : "";
     return (
         <ToolButton>
             <Icon path={mdiFormatText} size={0.75}/>
-            {variant}
+            {label}
             <Icon path={mdiMenuDown} size={0.75}/>
         </ToolButton>
     );
