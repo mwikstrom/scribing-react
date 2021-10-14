2850d4738c8ba2aa69bf5b736d9b9ef279f110bd
Mårten Wikström <5657237+mwikstrom@users.noreply.github.com>
2021-10-14 17:19:21 +0200

    remove variant label

--- src/internal/tools/ParagraphVariantButton.tsx
+++ src/internal/tools/ParagraphVariantButton.tsx
@@ -4,19 +4,24 @@ import { ToolButton } from "./ToolButton";
 import { mdiFormatText, mdiMenuDown } from "@mdi/js";
 import { ToolbarProps } from "./Toolbar";
 import { PARAGRAPH_STYLE_VARIANTS } from "scribing";
-import { ParagraphVariantLabel } from "./ParagraphVariantLabel";
+import { useFlowLocale } from "../../FlowLocaleScope";
+import { getParagraphVariantLocaleKey } from "../..";
 
 export const ParagraphVariantButton: FC<ToolbarProps> = ({commands}) => {
+    const locale = useFlowLocale();
     const variant = commands.getParagraphVariant();
     return (
         <ToolButton>
             <Icon path={mdiFormatText} size={0.75}/>
-            <span>
+            <span style={{ margin: "0 0.5rem" }}>
                 {PARAGRAPH_STYLE_VARIANTS.map(option => (
-                    <ParagraphVariantLabel
+                    <div 
                         key={option}
-                        variant={option}
-                        collapsed={option !== variant}
+                        style={option === variant ? undefined : {
+                            overflow: "hidden",
+                            height: 0,
+                        }}
+                        children={locale[getParagraphVariantLocaleKey(option)]}
                     />
                 ))}
             </span>
