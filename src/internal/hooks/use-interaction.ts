813cf785200ca4638436195d6f8e522584b55f55
Mårten Wikström <5657237+mwikstrom@users.noreply.github.com>
2021-10-26 13:26:54 +0200

    shift key negates ctrl key for enabling interaction

--- src/internal/hooks/use-interaction.ts
+++ src/internal/hooks/use-interaction.ts
@@ -5,7 +5,7 @@ import { useFlowLocale } from "../FlowLocaleScope";
 import { useInteractionInvoker } from "./useInteractionInvoker";
 import { useShowTip } from "../TooltipScope";
 import { useHover } from "./use-hover";
-import { useCtrlKey } from "./use-modifier-key";
+import { useCtrlKey, useShiftKey } from "./use-modifier-key";
 import { useNativeEventHandler } from "./use-native-event-handler";
 
 /** @internal */
@@ -26,12 +26,13 @@ export function useInteraction(
     const editMode = useEditMode();
     const hover = useHover(rootElem);
     const ctrlKey = useCtrlKey();
+    const shiftKey = useShiftKey();
     const invokeAction = useInteractionInvoker(interaction);
     const showTip = useShowTip();
     const locale = useFlowLocale();
     const [pending, setPending] = useState<Promise<void> | null>(null);
     const [error, setError] = useState<Error | null>(sourceError);
-    const clickable = !!interaction && (!editMode || (hover && ctrlKey));
+    const clickable = !!interaction && (!editMode || (hover && ctrlKey && !shiftKey));
     const message = useMemo(
         () => error ? (
             error.message
