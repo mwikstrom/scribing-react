2cf8b354d3d994da19d70f30241972bad1e12041
Mårten Wikström <5657237+mwikstrom@users.noreply.github.com>
2021-11-28 23:03:28 +0100

    client/server story

---
+++ stories/ClientServer.stories.tsx
@@ -0,0 +1,71 @@
+import React, { CSSProperties, useMemo } from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { FlowEditor } from "../src/FlowEditor";
+import { FlowSyncServer } from "scribing-server";
+import { FlowContent, FlowSyncProtocol } from "scribing";
+import { useFlowEditorClient } from "../src";
+
+export default {
+    title: "ClientServer",
+    component: FlowEditor,
+} as ComponentMeta<typeof FlowEditor>;
+
+const initialContent = FlowContent.fromJsonValue([
+    "This is the initial server content",
+    { break: "para" },
+]);
+  
+const Template: ComponentStory<typeof FlowEditor> = args => {
+    const server = useMemo(() => new FlowSyncServer({ initialContent }), []);
+    const protoA = useMemo(() => createTestProtocol(server, "A"), [server]);
+    const protoB = useMemo(() => createTestProtocol(server, "B"), [server]);
+    const clientA = useFlowEditorClient(protoA, { clientKey: "A" });
+    const clientB = useFlowEditorClient(protoB, { clientKey: "B" });
+
+    const wrapperStyle = useMemo<CSSProperties>(() => ({
+        display: "flex",
+        flexDirection: "row",
+        height: "calc(100vh - 2rem - 2px)",
+        border: "1px solid #888",
+    }), []);
+
+    const editorAStyle = useMemo<CSSProperties>(() => ({
+        flex: 0.5,
+        overflow: "auto",
+    }), []);
+
+    const editorBStyle = useMemo<CSSProperties>(() => ({
+        flex: 0.5,
+        overflow: "auto",
+        borderLeft: clientA.state !== null && clientB.state !== null ? "1px solid #888" : "none",
+    }), [clientA.state !== null && clientB.state !== null]);
+
+    return (
+        <div style={wrapperStyle}>
+            {clientA.state && (
+                <FlowEditor 
+                    {...args}
+                    style={editorAStyle}
+                    state={clientA.state}
+                    onStateChange={clientA.apply}
+                />
+            )}
+            {clientB.state && (
+                <FlowEditor 
+                    {...args}
+                    style={editorBStyle}
+                    state={clientB.state}
+                    onStateChange={clientB.apply}
+                />
+            )}
+        </div>
+    );
+};
+
+export const AutoSync = Template.bind({});
+AutoSync.args = {};
+
+const createTestProtocol = (server: FlowSyncServer, user: string): FlowSyncProtocol => ({
+    read: () => server.read(),
+    sync: input => server.sync(input, user),
+});
