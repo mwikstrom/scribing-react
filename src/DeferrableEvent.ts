8fbcb2d48f7823687a10438ee62e54b0c801b6ab
Mårten Wikström <5657237+mwikstrom@users.noreply.github.com>
2022-01-19 23:49:50 +0100

    pending

--- src/DeferrableEvent.ts
+++ src/DeferrableEvent.ts
@@ -3,13 +3,18 @@
  */
 export class DeferrableEvent {
     readonly #deferringCallbacks = new Set<() => Promise<void>>();
+    #pending = false;
 
     defer(callback: () => Promise<void>): void {
         this.#deferringCallbacks.add(callback);
+        this.#pending = true;
     }
+
+    public get pending(): boolean { return this.#pending; }
 
     /** @internal */
     async _complete(): Promise<void> {
         await Promise.all(Array.from(this.#deferringCallbacks).map(callback => callback()));
+        this.#pending = false;
     }
 }
