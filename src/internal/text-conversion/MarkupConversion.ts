3a7b0bce9eef0bfbc4746656752a0bb4cf77c3ef
Mårten Wikström <5657237+mwikstrom@users.noreply.github.com>
2023-03-14 09:00:24 +0100

    fix tag replacement at end of textrun

--- src/internal/text-conversion/MarkupConversion.ts
+++ src/internal/text-conversion/MarkupConversion.ts
@@ -4,7 +4,6 @@ import {
     FlowContent,
     FlowCursor,
     FlowRange,
-    ParagraphBreak,
     StartMarkup,
     TextRun
 } from "scribing";
@@ -14,15 +13,17 @@ export const MarkupConversion: TextConversionHandler = {
     isTrigger: insertedText => insertedText === ">",
     applyTo: ({ content, position, theme, select }) => {
         let cursor = new FlowCursor(content).move(position);
+        let delta = 0;
         
-        if (cursor.node instanceof ParagraphBreak) {
+        if (!(cursor.node instanceof TextRun)) {
             cursor = cursor.move(-1);
+            delta = 1;
         }
         
         const { node, offset } = cursor;
 
         if (node instanceof TextRun) {
-            const text = node.text.substring(0, offset);
+            const text = node.text.substring(0, offset + delta);
             const focus = cursor.position - cursor.offset + text.length;
             const match = /<(\/)?([a-z]+) *(\/)?>$/i.exec(text);
             if (match) {
