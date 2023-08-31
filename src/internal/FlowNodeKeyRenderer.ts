930a633a8bd7adbc4cca4d8dfab841ac57fc75b9
Mårten Wikström <5657237+mwikstrom@users.noreply.github.com>
2023-08-31 08:05:55 +0200

    reuse node key for start/empty markup nodes

--- src/internal/FlowNodeKeyRenderer.ts
+++ src/internal/FlowNodeKeyRenderer.ts
@@ -1,4 +1,4 @@
-import { BoxStyle, FlowBox, FlowNode, FlowTable, TableStyle } from "scribing";
+import { BoxStyle, EmptyMarkup, FlowBox, FlowNode, FlowTable, StartMarkup, TableStyle, TextStyle } from "scribing";
 import { FlowNodeKeyManager } from "./FlowNodeKeyManager";
 
 /** @internal */
@@ -19,10 +19,13 @@ export class FlowNodeKeyRenderer {
     }
 }
 
-export type NodeMapKey = FlowNode | BoxStyle | TableStyle;
+export type NodeMapKey = FlowNode | BoxStyle | TableStyle | TextStyle;
 
 export const getMapKey = (node: FlowNode): NodeMapKey => {
-    if (node instanceof FlowBox || node instanceof FlowTable) {
+    if (node instanceof FlowBox ||
+        node instanceof FlowTable ||
+        node instanceof StartMarkup ||
+        node instanceof EmptyMarkup) {
         return node.style;
     } else {
         return node;
